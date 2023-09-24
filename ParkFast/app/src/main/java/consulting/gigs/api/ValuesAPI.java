4c39d3ad9e81aee920cb4a776f1c7620f37eb4fa
Santiago Jaimes Pinilla <sjaimes@uniempresarial.edu.co>
2023-09-24 17:12:33 -0500

    codigo kellyn
    el codigo de la profesora

---
+++ ParkFast/app/src/main/java/consulting/gigs/api/ValuesAPI.java
@@ -0,0 +1,9 @@
+package consulting.gigs.api;
+
+public abstract interface ValuesAPI {
+
+ //String BASE_URL = "http://192.168.20.59/apiGigs/";
+ String BASE_URL = "http://10.57.24.12/apiGigs/";
+
+
+}
