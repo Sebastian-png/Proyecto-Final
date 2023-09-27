3f6bc44c88f8b24b0e6f6d510d027941b96ecc7a
Sebastian Ballen Loaiza <sballen@uniempresarial.edu.co>
2023-09-27 09:51:53 -0500

    cambio

--- Park_Fast/app/src/main/java/edu/co/park_fast/api/ValuesAPI.java
+++ Park_Fast/app/src/main/java/edu/co/park_fast/api/ValuesAPI.java
@@ -3,7 +3,7 @@ package edu.co.park_fast.api;
 public abstract interface ValuesAPI {
 
     //String BASE_URL = "http://192.168.20.59/apiGigs/";
-    String BASE_URL = "http://192.168.0.8/moviles-backend/";
+    String BASE_URL = "http://192.168.0.8:8080/moviles-backend/";
 
 
 }
